1d64c8ed5b779afb56ff8d9487cc276daddffe53
Sebastian Geidies <geidies@gmail.com>
2012-06-05 17:40:21 +0200

    initial version of python manifest

---
+++ manifests/init.pp
@@ -0,0 +1,44 @@
+# Class: Python
+#
+# Installs python from source. Needs the puppet-archive module.
+#
+# Usage:
+# include python
+# Or:
+# class { python: version => x.x.x }
+
+class python ( $version => 2.7 ) {
+  
+  archive { "python-${version}":
+    url => 'wget http://python.org/ftp/python/${version}/Python-${version}.tgz"
+    target => "/usr/local/src",
+    src_target => "/usr/local/src"
+  }
+
+  exec { "configure python ${version}":
+    cwd     => "/usr/local/src/Python-${version}",
+    command => "./configure",
+    require => Archive["python-${version}"],
+    alias   => "conf_python_${version}",
+  }
+
+  exec { "make && make install for python ${version}":
+    cwd     => "/usr/local/src/Python-${version}",
+    command => "make && make install",
+    require => Exec["conf_python_${version}"],
+    alias   => "install_python_${verion}"
+  }
+
+  exec { "move a possible already installed version of python":
+    onlyif => "test ! -L /usr/bin/python",
+    command => "mv /usr/bin/python /usr/bin/python`python -V 2>&1 | cut -d ' ' -f 2`"
+  }
+
+  file { "/usr/bin/python":
+    ensure => link,
+    target => "/usr/local/bin/python${version}",
+    force  => true,
+    require => Exec["install_python_${version}"],
+  }
+
+}
